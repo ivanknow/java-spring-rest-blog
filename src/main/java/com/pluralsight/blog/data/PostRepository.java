0c9de0801ebf9a041238bddb4ee9da890bb4206b
Ivan Rodrigues <ivanknow@gmail.com>
2021-10-11 17:46:47 -0300

    day one

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -1,6 +1,16 @@
 package com.pluralsight.blog.data;
 
-import com.pluralsight.blog.model.Author;
+import com.pluralsight.blog.model.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+    @RestResource(rel="contains-title", path="containsTitle")
+    List<Post> findByTitleContaining(String title);
+}
+/*import com.pluralsight.blog.model.Author;
 import com.pluralsight.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Component;
@@ -26,4 +36,4 @@ public class PostRepository {
     public Optional<Post> findById(Long id) {
         return null;
     }
-}
+}*/
